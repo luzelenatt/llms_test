d084ab79f56c543d997e2293b5f50e162e8a1281
Luz Elena Torres Talaverano <luzelena.torres@unmsm.edu.pe>
2024-04-04 17:47:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
 
-st.title("Codebasics Q&A 🌱")
-btn = st.button("Create Knowledgebase")
+st.title("LLMS 🌱")
+#btn = st.button("Create Knowledgebase")
 if btn:
     create_vector_db()
 
